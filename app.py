b0b1a1d16dc09399577dc50a60f7ff378a23ee17
Joe <josephedwardwork@gmail.com>
2020-06-01 21:09:32 -0400

    commit

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ from flask_pymongo import PyMongo
 import pandas as pd
 from scripts import load_global
 
-
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/covid_app"
 mongo = PyMongo(app)
 
 
 
-@app.route("/set_global_deaths_json/", methods=['GET'])
-def data_json():
+@app.route("/set_global_deaths_dict/", methods=['GET'])
+def data_to_mongo():
     global_deaths= mongo.db.global_deaths
     data_df = load_global.global_death_df()
     global_deaths.insert_many(data_df.to_dict("records"));
@@ -19,9 +18,13 @@ def data_json():
 
 # needs to pull from PyMongo
 @app.route("/get_global_deaths_json/", methods=['GET'])
-def data_html():
+def data_from_mongo():
     data_df = load_global.global_death_df()
+    # global_deaths= mongo.db.global_deaths
+    # global_deaths.find_all()
     return data_df.to_json();
+
+
 
 
 @app.route("/get_global_deaths_display/", methods=['GET'])
