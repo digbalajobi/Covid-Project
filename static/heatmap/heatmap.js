097c92998703af6228440362728e032f723f2464
Joe <josephedwardwork@gmail.com>
2020-06-04 23:19:32 -0400

    commit

---
+++ static/heatmap/heatmap.js
@@ -0,0 +1,57 @@
+var myMap = L.map("map", {
+    center: [37.5407, -77.4360],
+    zoom: 5
+  });
+  
+  console.log("mbKey -> heatmap.js:")
+  console.log(mbKey)
+  
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: 'mapbox/streets-v11',
+//   need to move into config var 
+  accessToken: mbKey
+  }).addTo(myMap);
+  
+  var date = "5/1/20"
+  var path = "/get_confirmed_us"
+  
+  d3.csv(path).then(function(data){
+
+    console.log(data[0])
+    var dateArray=[];
+    Object.entries(data[0]).forEach(([key,value])=>{
+      const CODE_PATTERN = /^([0-9]{1,2}.[0-9]{1,2}.[0-9]{2})$/;
+      const validateCode = function(key) {
+      return CODE_PATTERN.test(key);
+      };
+      const isValidCode = validateCode(key);
+      if(isValidCode===true)
+      dateArray.push(key);
+      });
+    
+    console.log(dateArray);
+    
+    
+    var heatArray = [];
+    for (var i = 0; i < data.length; i++) {  
+           
+         heatArray.push([parseInt(data[i].Lat), 
+                         parseInt(data[i].Long_), 
+                         parseInt(data[i][date])]);
+  
+      }
+    console.log(heatArray)
+  
+    var heat = L.heatLayer(heatArray, {
+      radius: 15,
+      blur: 15
+    }).addTo(myMap);
+    });
+  
+  
+  
