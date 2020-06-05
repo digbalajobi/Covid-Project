59a6c3a60367c6d7cd52f3f83a8e77812a455768
Joe <josephedwardwork@gmail.com>
2020-06-05 00:12:52 -0400

    commit

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ is_prod =os.environ['PWD'] == "/app"
 if(is_prod):
     print("is prod")
     app.config["MAPBOX_KEY"] = os.environ['MAPBOX_KEY']
-    uri =  os.environ['MONGODB_URI']
-    # +"&retryWrites=false"
+    uri =  os.environ['MONGODB_URI'] +"&retryWrites=false"
     client = MongoClient(uri,
                      connectTimeoutMS=30000,
                      socketTimeoutMS=None,
